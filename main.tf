868bc263c1cb2ab601b3ac448c950d4fb59a8197
saifeddine Rajhi <rajhiseif@gmail.com>
2023-11-15 22:47:05 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    dmsnitch = {
+      source = "plukevdh/dmsnitch"
+      version = "0.1.5"
+    }
+    vault = {
+      source  = "hashicorp/vault"
+      version = "3.12.0"
+    }
+  }
+}
+
+provider "dmsnitch" {
+  api_key = var.dms_key
+}
+
+resource "dmsnitch_snitch" "dmsnitch" {
+  name = var.snitch_name
+  notes = "DeadMan's Switch ensures proper functioning of k8s Alertmanager alerting pipeline"
+
+  interval = var.snitch_interval
+  type = var.snitch_type
+  tags = var.snitch_tags
+  alert_email = var.snitch_emails
+}
