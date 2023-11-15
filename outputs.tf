d2eac0d4ea09bd28b34d32106624702a4fa37adc
saifeddine Rajhi <rajhiseif@gmail.com>
2023-11-15 22:53:15 +0100

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "dmsnitch_id" {
+  value = dmsnitch_snitch.dmsnitch.id
+}
+
+output "dmsnitch_url" {
+  value = dmsnitch_snitch.dmsnitch.url
+}
+
+output "dmsnitch_status" {
+  value = dmsnitch_snitch.dmsnitch.status
+}
